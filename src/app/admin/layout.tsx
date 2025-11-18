0ba0c58d8a7a9724ce8ecc8eccc63d515eefbdc3
Tawan BerkFah <lek.26015@gmail.com>
2025-11-18 16:39:40 +0000

    ส่วนของ เนื้อหาขยับได้ แต่เมนูของแดมินไม่ต้องขยับ

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -40,7 +40,7 @@ export default function AdminLayout({ children }: { children: React.ReactNode })
     }
 
   return (
-    <div className="grid min-h-screen w-full md:grid-cols-[280px_1fr] lg:grid-cols-[280px_1fr]">
+    <div className="grid h-screen w-full md:grid-cols-[280px_1fr] lg:grid-cols-[280px_1fr]">
       <div className="hidden border-r bg-muted/40 md:block">
         <div className="flex h-full max-h-screen flex-col gap-2">
           <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
@@ -97,7 +97,7 @@ export default function AdminLayout({ children }: { children: React.ReactNode })
           </div>
         </div>
       </div>
-      <div className="flex flex-col">
+      <div className="flex flex-col overflow-auto">
         {children}
       </div>
     </div>
