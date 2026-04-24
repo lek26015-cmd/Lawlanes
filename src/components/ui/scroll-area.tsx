f7bb2db671c1d589bbafd9fd24a0e64c57d03010
lek26015-cmd <lek.26015@gmail.com>
2026-04-25 04:55:08 +0700

    fix: explicitly define ScrollAreaProps for better TS support

--- src/components/ui/scroll-area.tsx
+++ src/components/ui/scroll-area.tsx
@@ -5,9 +5,13 @@ import * as ScrollAreaPrimitive from "@radix-ui/react-scroll-area"
 
 import { cn } from "@/lib/utils"
 
+export interface ScrollAreaProps extends React.ComponentPropsWithoutRef<typeof ScrollAreaPrimitive.Root> {
+  viewportClassName?: string
+}
+
 const ScrollArea = React.forwardRef<
   React.ElementRef<typeof ScrollAreaPrimitive.Root>,
-  React.ComponentPropsWithoutRef<typeof ScrollAreaPrimitive.Root> & { viewportClassName?: string }
+  ScrollAreaProps
 >(({ className, children, viewportClassName, ...props }, ref) => (
   <ScrollAreaPrimitive.Root
     ref={ref}
